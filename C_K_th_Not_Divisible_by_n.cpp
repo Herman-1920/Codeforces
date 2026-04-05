774269e50624a1ae4c644e14adbf61a6d0b337d8
Herman-1920 <sadik38003@gmail.com>
2026-04-05 21:07:18 +0600

    Added problem 1352C

---
+++ C_K_th_Not_Divisible_by_n.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;cin>>t;
+    while(t--){
+        ll n,k;cin>>n>>k;
+        ll l=1,r=1e18,mid,ans;
+        while(l<=r){
+            mid=l+(r-l)/2;
+            if((mid-(mid/n))>=k){
+                ans=mid;
+                r=mid-1;
+            }
+            else l=mid+1;
+        }
+        cout<<ans<<"\n";
+    }
+
+}
