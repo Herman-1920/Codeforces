fecff8c09850b30f14845f5b16d24e81bdf090eb
Herman-1920 <sadik38003@gmail.com>
2026-04-14 22:27:40 +0600

    Added A.Chef and fruits

---
+++ A_Chef_and_Fruits.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;cin>>t;
+    while(t--){
+        int n,m,k;cin>>n>>m>>k;
+
+        cout<<max(0,abs(n-m)-k)<<"\n";
+    }
+
+}
